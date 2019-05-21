b8532a73291c4d9ff14f44278828997f33fc0d70
Sorin Popovici <sorin.popovici@cgm.com>
2019-05-21 19:09:11 +0300

    added new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,22 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {RecipesComponent} from './recipes/recipes.component';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
-import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
-import {DropdownDirective} from './shared/dropdown.directive';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
-import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRouting } from './app-routing.module';
-import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthService } from './auth/auth.service';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthGuard } from './auth/auth-guard.service';
+import { RecipesModule } from './recipes/recipe.module';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -31,14 +25,7 @@ import { AuthGuard } from './auth/auth-guard.service';
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeListComponent,
-    RecipesComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingEditComponent,
-    DropdownDirective,
-    RecipesStartComponent,
-    RecipeEditComponent,
     SignupComponent,
     SigninComponent
   ],
@@ -46,9 +33,9 @@ import { AuthGuard } from './auth/auth-guard.service';
     BrowserModule,
     FormsModule,
     HttpModule,
+    RecipesModule,
     AppRouting,
-    RouterModule,
-    ReactiveFormsModule
+    SharedModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
---
+++ src/app/recipes/recipe.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipesStartComponent } from "./recipes-start/recipes-start.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { RecipesRoutingModule } from "./recipe-routing.module";
+import { SharedModule } from "../shared/shared.module";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipesStartComponent,
+        RecipeListComponent,
+        RecipeEditComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule,
+        SharedModule
+    ]
+})
+export class RecipesModule {
+
+}
