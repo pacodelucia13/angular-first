f0349405286a1e32d35ff6915b62e250f3831ddc
Sorin Popovici <sorin.popovici@cgm.com>
2019-05-31 19:27:56 +0300

    latest modifications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from "./core/home/home.component";
 import { RecipesComponent } from "./recipes/recipes.component";
+import { AuthGuard } from "./auth/auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuard]},
     { path: 'shopping-list', component: ShoppingListComponent }
 ]
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers: [AuthGuard]
 })
-export class AppRouting { }
+export class AppRoutingModule { }
--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,13 +1,22 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad } from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
+    
     constructor(private authService: AuthService, private router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const allowedOnPage = this.authService.isAuthenticated();
+        if (!allowedOnPage) {
+            this.router.navigate(['/signin']);
+        }
+        return allowedOnPage;
+    }
+
+    canLoad(route: import("@angular/router").Route): boolean | Observable<boolean> | Promise<boolean> {
         const allowedOnPage = this.authService.isAuthenticated();
         if (!allowedOnPage) {
             this.router.navigate(['/signin']);
