55689db95275927b32eedc62474cd7eaa7eb4c55
Sorin Popovici <sorin.popovici@cgm.com>
2019-06-20 18:42:09 +0300

    redo auth

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,10 +2,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { SignupComponent } from "./signup/signup.component";
 import { SigninComponent } from "./signin/signin.component";
+import { AuthComponent } from "./auth.component";
 
 const authRoutes: Routes = [
     { path: 'signup', component: SignupComponent },
-    { path: 'signin', component: SigninComponent }
+    { path: 'signin', component: SigninComponent },
+    { path: 'auth', component: AuthComponent}
 ];
 @NgModule({
     imports: [
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { SigninComponent } from "./signin/signin.component";
 import { SignupComponent } from "./signup/signup.component";
 import { FormsModule } from "@angular/forms";
 import { AuthRoutingModule } from "./auth-routing.module";
+import { AuthComponent } from "./auth.component";
 
 @NgModule({
     declarations: [
         SigninComponent,
-        SignupComponent
+        SignupComponent,
+        AuthComponent
     ],
     imports: [
         FormsModule,
